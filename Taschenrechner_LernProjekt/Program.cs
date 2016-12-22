0485b0dbdaf8edd620aae24fb402197a7bf79d2f
Daniel Bradtmöller <d.bradtmoeller@hotmail.de>
2016-12-22 23:04:40 +0100

    Erstellung einer Methode für die Berechnung

--- Taschenrechner_LernProjekt/Program.cs
+++ Taschenrechner_LernProjekt/Program.cs
@@ -20,29 +20,86 @@ namespace Taschenrechner_LernProjekt
             string zweiteZahlAslsString = HoleBenutzerEingabe("Bitte zweiten Wert eingeben: ");
 
             //Verwendung der Methode HoleBenutzerEingabe mit Text und Rückgabe
-            string operatoren = HoleBenutzerEingabe("Bitte Operation auswählen angeben ( + - / *): ");
+            string operatoren = HoleBenutzerEingabe("Bitte Operation auswählen angeben ( + , - , / , *): ");
 
 
             //Werte von String zu Double konvertierten
             double ersteZahl = Convert.ToDouble(ersteZahlAlsString);
             double zweiteZahl = Convert.ToDouble(zweiteZahlAslsString);
 
+            double ergebnis = Berechnung(ersteZahl, zweiteZahl, operatoren);
+
+            //double ergebnis = 0;
+
+            //switch (operatoren)
+            //{
+                            
+            //    case "+" : //addieren beiden Werte mit der Methode Addieren
+            //        ergebnis = Addiere(ersteZahl, zweiteZahl);
+                   
+            //        //Ausgabe des Ergebnis
+            //        Console.WriteLine("Ergebnis: {0} ", ergebnis);
+            //        break;
+
+            //    case "-":
+            //        ergebnis = Subtrahiere(ersteZahl, zweiteZahl);
+            //            //Ausgabe des Ergebnis
+            //         Console.WriteLine("Ergebnis: {0} ", ergebnis);
+            //        break;
+
+            //    case "*":
+            //        ergebnis = Multipliziere(ersteZahl, zweiteZahl);
+            //        //Ausgabe des Ergebnis
+            //        Console.WriteLine("Ergebnis: {0} ", ergebnis);
+            //        break;
+
+            //    case "/":
+            //        ergebnis = Dividiere(ersteZahl, zweiteZahl);
+            //        //Ausgabe des Ergebnis
+            //        Console.WriteLine("Ergebnis: {0} ", ergebnis);
+
+            //        break;
+
+
+            //    default:
+            //        Console.WriteLine("Ungültige Operation, Abbruch");
+
+            //        break;
+            //}
+
+         HoleBenutzerEingabe("Bitte zum beenden Return drücken");
+
+        }
+
+        //Methode HoleBenutzerEingabe
+
+        static string HoleBenutzerEingabe(string auforderungSummanden)
+        {
+            Console.WriteLine(auforderungSummanden);
+            string summand = Console.ReadLine();
+
+            return summand;
+
+        }
+
+        static double Berechnung(double ersteZahl, double zweiteZahl, string operatoren)
+        {
             double ergebnis = 0;
 
             switch (operatoren)
             {
-                            
-                case "+" : //addieren beiden Werte mit der Methode Addieren
+
+                case "+": //addieren beiden Werte mit der Methode Addieren
                     ergebnis = Addiere(ersteZahl, zweiteZahl);
-                   
+
                     //Ausgabe des Ergebnis
                     Console.WriteLine("Ergebnis: {0} ", ergebnis);
                     break;
 
                 case "-":
                     ergebnis = Subtrahiere(ersteZahl, zweiteZahl);
-                        //Ausgabe des Ergebnis
-                     Console.WriteLine("Ergebnis: {0} ", ergebnis);
+                    //Ausgabe des Ergebnis
+                    Console.WriteLine("Ergebnis: {0} ", ergebnis);
                     break;
 
                 case "*":
@@ -64,20 +121,7 @@ namespace Taschenrechner_LernProjekt
 
                     break;
             }
-
-         HoleBenutzerEingabe("Bitte zum beenden Return drücken");
-
-        }
-
-        //Methode HoleBenutzerEingabe
-
-        static string HoleBenutzerEingabe(string auforderungSummanden)
-        {
-            Console.WriteLine(auforderungSummanden);
-            string summand = Console.ReadLine();
-
-            return summand;
-
+            return ergebnis;
         }
 
         //Methode Addiere mit Rückgabewert
